87bd90cc9a6bddb4172a2c2d1a2fc157b4e7f980
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-03-28 08:57:54 +0100

    Token Validity Length Extended

--- server/src/tokens.js
+++ server/src/tokens.js
@@ -4,13 +4,13 @@ const { sign } = require("jsonwebtoken");
 // ----------------------------------
 const createAccessToken = userId => {
   return sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "15m"
+    expiresIn: "14d"
   });
 };
 
 const createRefreshToken = userId => {
   return sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: "7d"
+    expiresIn: "14d"
   });
 };
 
